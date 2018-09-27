7232e92a2d8c8a09cf519e20e8f70973c2b11f06
Tim Pizey <pizeyt@elsevier.com>
2018-09-27 11:51:15 +0100

    Onward

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 Filter a properties file.
 
-Prove that the values are available to Mavn at build time.
+Prove that the values are available to Maven at build time.
+
+https://maven.apache.org/plugins/maven-resources-plugin/examples/filter.html
+https://maven.apache.org/settings.html
+https://maven.apache.org/ref/3.5.4/maven-settings/settings.html
+https://maven.apache.org/examples/injecting-properties-via-settings.html
+
+Yet another dependency:
+https://github.com/shyiko/servers-maven-extension
+
+To get tests to pass add
+''''
+<profiles>
+  <profile>
+    <id>filter</id>
+    <activation><activeByDefault>true</activeByDefault></activation>
+    <properties>
+      <filter.key1>firstKeyValue</filter.key1>
+    </properties>
+  </profile>
+</profiles>
+''''
--- src/test/java/uk/pizey/tim/MainTest.java
+++ src/test/java/uk/pizey/tim/MainTest.java
@@ -1,7 +1,14 @@
 package uk.pizey.tim;
 
 import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.assertNull;
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
+
+import java.io.InputStream;
+import java.util.Properties;
 
 
 public class MainTest {
@@ -13,9 +20,23 @@ public class MainTest {
      *
      */
     @Test
-    public void shouldCreateMainObject(){
-        Main main = new Main();
-        assertNotNull("Main method called.", main);
+    public void testFilteredProperties() throws Exception{
+        Properties p = new Properties();
+        InputStream input = this.getClass().getResourceAsStream(
+                "filtered.properties");
+        assertNotNull(input);
+        p.load(input);
+        assertNull(p.getProperty("undefinedKey"));
+
+        assertEquals("${notDefined}", p.getProperty("missing"));
+        assertEquals("${env.notDefined}", p.getProperty("missingEnv"));
+        assertEquals("${settings.notDefined}", p.getProperty("missingSetting"));
+
+        assertEquals("defined", p.getProperty("defined"));
+        assertNotNull(p.getProperty("home"));
+        assertTrue(p.getProperty("localRepository").endsWith("repository"));
+        assertEquals("firstKeyValue", p.getProperty("key1"));
+
     }
 }
 
